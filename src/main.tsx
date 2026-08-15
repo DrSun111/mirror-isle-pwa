872f50c147096d5f70b47460eb2f17fc0a9514e9
DrSun111 <2805051912@qq.com>
2026-08-15 14:03:00 +0800

    feat: mount drift reply inbox

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import ExperienceV014 from './ExperienceV014.tsx'
+import DriftInboxLayer from './DriftInboxLayer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <>
       <App />
       <ExperienceV014 />
+      <DriftInboxLayer />
     </>
   </StrictMode>,
 )
