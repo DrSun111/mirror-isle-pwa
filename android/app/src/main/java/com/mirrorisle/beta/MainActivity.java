46da3a81c74ac7c0c13023c92bb53433ebd0157e
DrSun111 <2805051912@qq.com>
2026-08-16 10:33:25 +0800

    fix: add native WebView boot readiness watchdog

--- android/app/src/main/java/com/mirrorisle/beta/MainActivity.java
+++ android/app/src/main/java/com/mirrorisle/beta/MainActivity.java
@@ -1,5 +1,60 @@
 package com.mirrorisle.beta;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+
+import com.getcapacitor.Bridge;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    private static final String BOOT_TAG = "MirrorBoot";
+    private static final int MAX_BOOT_CHECKS = 40;
+    private static final long BOOT_CHECK_INTERVAL_MS = 250L;
+
+    private final Handler bootHandler = new Handler(Looper.getMainLooper());
+    private int bootChecks = 0;
+
+    private final Runnable bootCheck = new Runnable() {
+        @Override
+        public void run() {
+            Bridge bridge = getBridge();
+            if (bridge == null) {
+                scheduleNextBootCheck();
+                return;
+            }
+
+            bridge.eval("document.documentElement.dataset.mirrorReady || ''", value -> {
+                if ("\"1\"".equals(value) || "1".equals(value)) {
+                    Log.i(BOOT_TAG, "MIRROR_NATIVE_BOOT_READY");
+                    bootHandler.removeCallbacks(bootCheck);
+                    return;
+                }
+                scheduleNextBootCheck();
+            });
+        }
+    };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        bootChecks = 0;
+        bootHandler.removeCallbacks(bootCheck);
+        bootHandler.post(bootCheck);
+    }
+
+    @Override
+    public void onPause() {
+        bootHandler.removeCallbacks(bootCheck);
+        super.onPause();
+    }
+
+    private void scheduleNextBootCheck() {
+        bootChecks += 1;
+        if (bootChecks >= MAX_BOOT_CHECKS) {
+            Log.e(BOOT_TAG, "MIRROR_NATIVE_BOOT_TIMEOUT");
+            return;
+        }
+        bootHandler.postDelayed(bootCheck, BOOT_CHECK_INTERVAL_MS);
+    }
+}
