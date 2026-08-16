3d60ca163b0bce4d61b6402497695f7d179c5819
DrSun111 <2805051912@qq.com>
2026-08-16 14:29:15 +0800

    feat: route Supabase password recovery into v2 reset screen

---
+++ src/MirrorV2Root.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import MirrorV2Experience from './MirrorV2Experience'
+import PasswordRecoveryV2 from './PasswordRecoveryV2'
+import { cleanSupabase, restoreV2Account } from './mirrorV2Api'
+
+function recoveryInUrl() {
+  return /(?:[?#&])type=recovery(?:[=&]|$)/.test(window.location.href)
+}
+
+export default function MirrorV2Root() {
+  const [recovering, setRecovering] = useState(recoveryInUrl)
+
+  useEffect(() => {
+    const { data } = cleanSupabase.auth.onAuthStateChange((event) => {
+      if (event === 'PASSWORD_RECOVERY') setRecovering(true)
+    })
+    return () => data.subscription.unsubscribe()
+  }, [])
+
+  if (recovering) {
+    return <PasswordRecoveryV2 onDone={async () => {
+      await restoreV2Account().catch(() => null)
+      setRecovering(false)
+      window.location.reload()
+    }}/>
+  }
+
+  return <MirrorV2Experience />
+}
